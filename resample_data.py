10726d405f09c94ef74173612c28e5c3feac6816
heliar <heliar@zju.edu.cn>
2017-05-25 19:35:00 +0800

    add testa to hdf5 methos

--- resample_data.py
+++ resample_data.py
@@ -167,11 +167,16 @@ def preprocessing(inputs, mean, std, is_train_set=False, only_calc_mean_std=Fals
         return datas
 def main():
     # Get the data.
-    trains = joblib.load("../data/CIKM2017_train/train_Imp_3x3.pkl")
+    #trains = joblib.load("../data/CIKM2017_train/train_Imp_3x3.pkl")
 
-    #testa_set = joblib.load("../data/CIKM2017_testA/testA_Imp_3x3.pkl")
+    testa_set = joblib.load("../data/CIKM2017_testA/testA_Imp_3x3.pkl")
+    testa_x = []
 
-    train_x, train_y, train_class = sample(trains)
+    for item in testa_set:
+        testa_x.append(item["input"])
+
+    testa_x = np.asarray(testa_x, dtype=np.int16)
+    #train_x, train_y, train_class = sample(trains)
     '''
     for i in range(10):
         np.random.shuffle(data_set)
@@ -190,13 +195,14 @@ def main():
         convert_to(testa_out, "testA_Imp_3x3_normalization_"+str(i)+"_fold", is_test=True)
     #joblib.dump(value=data_set, filename="../data/CIKM2017_train/train_Imp_3x3_classified_del_height_no.4.pkl",compress=3)
     '''
-    h5fname = "../data/CIKM2017_train/train_Imp_3x3_classified.h5"
+    h5fname = "../data/CIKM2017_testA/testA_Imp_3x3_classified.h5"
     import h5py
     "write file"
     with h5py.File(h5fname, "w") as f:
-        f.create_dataset(name="train_set_x", shape=train_x.shape, data=train_x, dtype=train_x.dtype, compression="lzf", chunks=True)
-        f.create_dataset(name="train_set_y", shape=train_y.shape, data=train_y, dtype=train_y.dtype, compression="lzf", chunks=True)
-        f.create_dataset(name="train_set_class", shape=train_class.shape, data=train_class, dtype=train_class.dtype, compression="lzf", chunks=True)
+        f.create_dataset(name="testa_set_x", shape=testa_x.shape, data=testa_x, dtype=testa_x.dtype, compression="lzf", chunks=True)
+        #f.create_dataset(name="train_set_x", shape=train_x.shape, data=train_x, dtype=train_x.dtype, compression="lzf", chunks=True)
+        #f.create_dataset(name="train_set_y", shape=train_y.shape, data=train_y, dtype=train_y.dtype, compression="lzf", chunks=True)
+        #f.create_dataset(name="train_set_class", shape=train_class.shape, data=train_class, dtype=train_class.dtype, compression="lzf", chunks=True)
 
     return
 if __name__ == "__main__":
